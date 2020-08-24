aeb9cf9f23b2e3466e98f6e8413c09ff15c5e73b
royyy1999 <royyy1999@CIS-RDS3>
2020-08-24 16:14:58 -0500

    Turorial C1

---
+++ VectorMath/Vector3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>
+/// Summary description for Vector3
+/// </summary>
+public class Vector3
+
+{
+	public Vector3()
+	{
+		//
+		// TODO: Add constructor logic here
+		//
+	}
+}
